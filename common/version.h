8b56365ad54add9bb44842edd9c33dc521f6fd07
schelli04 <martin@hohmann-online.org>
2015-01-30 14:43:11 +0100

    Gertbot in makefile ergänzt

--- common/version.h
+++ common/version.h
@@ -1,5 +1,5 @@
-const int revisionnr = 
-8470
-;
-const char* commithash = 
-"2b2bcca7efc13688f8c51beedee4a59eaef7b06a";
+"const int revisionnr = " 
+8538
+";" 
+"const char* commithash = " 
+"H"";" 
